a28492c30cd94131e80e76d8afce761265561055
MaiteA2005 <aldenkampmaite@gmail.com>
2024-11-07 20:25:31 +0100

    update

--- signup.php
+++ signup.php
@@ -9,35 +9,56 @@
     <link rel="stylesheet" href="./css/stylesheet.css">
 </head>
 <body>
-    <form action="" method="post">
-        <label for="first_name">First Name:</label>
-        <input type="text" id="first_name" name="first_name" required><br>
-
-        <label for="last_name">Last Name:</label>
-        <input type="text" id="last_name" name="last_name" required><br>
-
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required><br>
-
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required><br>
-
-        <label for="street_name">Street Name:</label>
-        <input type="text" id="street_name" name="street_name" required><br>
-
-        <label for="house_number">House Number:</label>
-        <input type="text" id="house_number" name="house_number" required><br>
-
-        <label for="postal_code">Postal Code:</label>
-        <input type="text" id="postal_code" name="postal_code" required><br>
-
-        <label for="city">City:</label>
-        <input type="text" id="city" name="city" required><br>
-
-        <label for="country">Country:</label>
-        <input type="text" id="country" name="country" required><br>
-
-        <input type="submit" value="Sign Up">
     </form>
+    <div class="websiteLogin">
+		<div class="form form--login">
+			<form action="" method="post">
+				<h2 form__title><a href="login.php">Log in</a></h2>
+				<h2 form__title>Sign up</h2>
+
+				<div class="form__field">
+					<label for="first_name">First Name</label>
+					<input type="text" name="first_name" required>
+				</div>
+				<div class="form__field">
+					<label for="last_name">Last Name</label>
+					<input type="text" name="last_name">
+				</div>
+                <div class="form__field">
+					<label for="Email">Email</label>
+					<input type="text" name="email">
+				</div>
+				<div class="form__field">
+					<label for="Password">Password</label>
+					<input type="password" name="password">
+				</div>
+                <div class="form__field">
+					<label for="street_name">Street name</label>
+					<input type="text" name="Street name">
+				</div>
+				<div class="form__field">
+					<label for="house_number">House number</label>
+					<input type="text" name="House number">
+				</div>
+                <div class="form__field">
+					<label for="postal_code">Postal code</label>
+					<input type="text" name="Postal code">
+				</div>
+				<div class="form__field">
+					<label for="city">City</label>
+					<input type="text" name="city">
+				</div>
+                <div class="form__field">
+					<label for="country">Country</label>
+					<input type="text" name="country">
+                </div>
+
+				<div class="form__field">
+					<input type="submit" value="Sign up" class="btn btn--primary">	
+				</div>
+                
+			</form>
+		</div>
+	</div>
 </body>
 </html>
