6cf88f42062958fd12864ef3503faf011024e8a0
MaiteA2005 <aldenkampmaite@gmail.com>
2024-11-27 12:18:00 +0100

    fix: autoload

--- bootstrap.php
+++ bootstrap.php
@@ -1,19 +1,13 @@
 <?php
-    //zijn de inlog gegevens oke
-    if (session_status() == PHP_SESSION_NONE) {
+//zijn de inlog gegevens oke
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 
-        session_start();
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+    header("Location: login.php");
+    exit;
+}
 
-    }
-
-    if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
-
-        header("Location: login.php");
-
-        exit;
-
-    } 
-
-
-   //include autoload
-   require_once(__DIR__ . '/vendor/autoload.php');
+//include autoload
+require_once(__DIR__ . '/../vendor/autoload.php');
