ce4e7e38d67ac1bd9336f110f37190a0711b5e22
MaiteA2005 <aldenkampmaite@gmail.com>
2024-11-27 11:38:16 +0100

    refactor: deployment database

--- classes/Db.php
+++ classes/Db.php
@@ -11,12 +11,21 @@
         public static function getConnection(){
             //Db :: getConnection();
             if(self::$conn == null){
-                
-                self::$conn = new PDO('mysql:host=localhost;dbname=webshop', 'root', '');
-                self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            
-            }
-            return self::$conn;
-        }
-    }
+                try{
+                    $host = "junction.proxy.rlwy.net";
+                    $dbname = "webshop";
+                    $user = "root";
+                    $pass = "OjNOfIRAMXFBTyIgNhbvmwPhrehxAyqH";
+                    $port = "19872";
+
+                    self::$conn = new PDO("mysql:host=$host;dbname=$dbname;port=$port", $user, $pass);
+                    self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    return self::$conn;
+                } catch(PDOException $e){
+                    echo "Connection failed: " . $e->getMessage();
+                    return null;
+                }
+            }else{
+            return self::$conn;}
+    }}
 ?>
